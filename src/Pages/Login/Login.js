9df385e6611bfb687aa69167da929887f4cb55e9
islammd39e <islammdrahat308@gmail.comm>
2022-10-22 13:16:49 +0600

    test commit

---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h2>Log in section</h2>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/Shared/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>header section</h2>
+        </div>
+    );
+};
+
+export default Header;
