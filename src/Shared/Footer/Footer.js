addd34526e5c593419951223fb4f5b0020bca4c3
islammd39e <islammdrahat308@gmail.comm>
2022-10-23 12:22:06 +0600

    some issus fix

--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <Footer className='text-center py-3 text-light bg-dark mt-3'>
+        <div className='text-center py-3 text-light bg-dark mt-3'>
             <p><small>© Copy Right</small></p>
-        </Footer>
+        </div>
     );
 };
 
